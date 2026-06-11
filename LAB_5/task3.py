eaa600ebb8e3131df6da036c625e1e572be033bd
Молния Маккуин <itismytwink32@gmail.com>
2026-06-11 13:44:00 +0300

    Update task3.py

--- LAB_5/task3.py
+++ LAB_5/task3.py
@@ -30,13 +30,13 @@ def main():
     print('START')
     df = pd.read_sql('SELECT USERNAME, USER_ACTION, ACTION_DATE, ACTION_TIME, ACTION_RESULT FROM USER_LOGS', engines[0])
     df.columns = ['username', 'user_action', 'action_date', 'action_time', 'action_result']
-    df['shard'] = pd.to_datetime(df['action_date']).dt.month - 1
+    df['shard'] = df.index % 12
     print('прочитано всего ' + str(len(df)) + ' записей')
     for i in range(12):
         shard_df = df[df['shard'] == i].drop(columns=['shard'])
         if not shard_df.empty:
             shard_df.to_sql('Logs', engines[i], if_exists='append', index=False, method='multi', chunksize=400)
-            print('DataBase ' + str(i + 1) + ' (месяц ' + str(i + 1) + '): ' + str(len(shard_df)) + ' записей')
+            print('DataBase ' + str(i + 1) + ' (шард ' + str(i + 1) + '): ' + str(len(shard_df)) + ' записей')
 
 if __name__ == '__main__':
     main()

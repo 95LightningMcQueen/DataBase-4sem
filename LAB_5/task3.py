0b22cee56691a1befe4989f51d450f8c5e46099b
Молния Маккуин <itismytwink32@gmail.com>
2026-06-17 14:58:25 +0300

    Add file via upload

--- LAB_5/task3.py
+++ LAB_5/task3.py
@@ -4,12 +4,14 @@ from sqlalchemy.orm import DeclarativeBase
 from config import adress, dbname
 
 
-DATABASE_URL = f'mssql+pyodbc://@{adress}/{dbname}?driver=ODBC+Driver+17+for+SQL+Server&Trusted_Connection=yes&Encrypt=No&TrustServerCertificate=yes'
+source_url = f'mssql+pyodbc://@{adress}/{dbname}?driver=ODBC+Driver+17+for+SQL+Server&Trusted_Connection=yes&Encrypt=No&TrustServerCertificate=yes'
+source_engine = create_engine(source_url)
 
 engines = []
-for i in range(12):
-    engine = create_engine(DATABASE_URL)
-    engines.append(engine)
+for i in range(1, 13):
+    shard_db_name = f"{dbname}_{i}" 
+    shard_url = f'mssql+pyodbc://@{adress}/{shard_db_name}?driver=ODBC+Driver+17+for+SQL+Server&Trusted_Connection=yes&Encrypt=No&TrustServerCertificate=yes'
+    engines.append(create_engine(shard_url))
 
 class Base(DeclarativeBase):
     pass
@@ -28,15 +30,15 @@ for engine in engines:
 
 def main():
     print('START')
-    df = pd.read_sql('SELECT USERNAME, USER_ACTION, ACTION_DATE, ACTION_TIME, ACTION_RESULT FROM USER_LOGS', engines[0])
+    df = pd.read_sql('SELECT USERNAME, USER_ACTION, ACTION_DATE, ACTION_TIME, ACTION_RESULT FROM USER_LOGS', source_engine)
     df.columns = ['username', 'user_action', 'action_date', 'action_time', 'action_result']
     df['shard'] = df.index % 12
-    print('прочитано всего ' + str(len(df)) + ' записей')
+    print(f'Прочитано из основной БД: {len(df)} записей')
     for i in range(12):
         shard_df = df[df['shard'] == i].drop(columns=['shard'])
         if not shard_df.empty:
-            shard_df.to_sql('Logs', engines[i], if_exists='append', index=False, method='multi', chunksize=400)
-            print('DataBase ' + str(i + 1) + ' (шард ' + str(i + 1) + '): ' + str(len(shard_df)) + ' записей')
+            shard_df.to_sql('User_Logs', engines[i], if_exists='append', index=False, method='multi', chunksize=400)
+            print(f'Узел {i + 1} (БД {dbname}_{i+1}): записано {len(shard_df)} записей')
 
 if __name__ == '__main__':
     main()
